23b9ab0a384f8549e5c332ac97c80a80b2e9092b
User <>
2023-03-07 21:32:37 +0900

    [kyk] test flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// 우리는 값을 저장할 "provider"를 만들겁니다(여기서 값은 "Hello world"를 의미합니다).
-// 프로바이더를 사용하는 것으로 값의 mock/override가 가능하게 됩니다.
-final helloWorldProvider = Provider((_) => 'Hello world');
-
 void main() {
   runApp(
-    // 위젯에서 프로바이더를 사용하고 읽기위해
-    // 앱 전체적으로 "ProviderScope" 위젯을 감싸줘야 합니다.
-    // 여기에 프로바이더의 상태가 저장됩니다.
-    ProviderScope(
+    /// [MyApp] is wrapped in a [ProviderScope].
+    /// This widget is where the state of most of our providers will be stored.
+    /// This replaces `MultiProvider` if you've used `provider` before.
+    const ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
-// Note: MyApp은 hooks_riverpod의 HookConsumerWidget을 상속받습니다.
-class MyApp extends HookConsumerWidget {
+/// A provider that creates and listen to a [StateNotifier].
+///
+/// Providers are declared as global variables.
+/// This does not hinder testability, as the state of a provider is instead
+/// stored inside a [ProviderScope].
+final counterProvider = StateNotifierProvider<Counter, int>((_) => Counter());
+
+/// A simple [StateNotifier] that implements a counter.
+///
+/// It doesn't have to be a [StateNotifier], and could be anything else such as:
+/// - [ChangeNotifier], with [ChangeNotifierProvider]
+/// - [Stream], with [StreamProvider]
+/// ...
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+
+  void increment() => state++;
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends HookConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
+    final count = ref.watch(counterProvider);
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: Center(
-          child: Text(value),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod counter example'),
+      ),
+      body: Center(
+        child: Text(
+          '$count',
+          style: Theme.of(context).textTheme.headlineMedium,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(counterProvider.notifier).increment(),
+        child: const Icon(Icons.add),
       ),
     );
   }
